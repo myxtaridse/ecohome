567e8dfe224d47e07463c0cdfe7e7a962fb44ddf
Ksenia Durkina <rustambyr69@gmail.com>
2024-11-02 11:23:46 +0300

    Update logo svg-2

--- src/components/HeaderMain/Navheader.tsx
+++ src/components/HeaderMain/Navheader.tsx
@@ -6,13 +6,13 @@ const Navheader = () => {
     <div className="header-navigation">
       <div
         className="header-nav-widget"
-        onClick={() => {
-          if (popupRef.current)
-            popupRef.current.style.transform =
-              popupRef.current.style.transform === "scaleY(1)"
-                ? "scaleY(0)"
-                : "scaleY(1)";
-        }}
+        // onClick={() => {
+        //   if (popupRef.current)
+        //     popupRef.current.style.transform =
+        //       popupRef.current.style.transform === "scaleY(1)"
+        //         ? "scaleY(0)"
+        //         : "scaleY(1)";
+        // }}
       >
         <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" fill="currentColor"><path d="M3 4H21V6H3V4ZM9 11H21V13H9V11ZM3 18H21V20H3V18Z"></path></svg>
       </div>
