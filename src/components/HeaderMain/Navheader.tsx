be6cfe00b008a65f2d369c70ea8fd4f0ebedd82e
Ksenia Durkina <rustambyr69@gmail.com>
2024-11-02 11:19:34 +0300

    Update logo svg

--- src/components/HeaderMain/Navheader.tsx
+++ src/components/HeaderMain/Navheader.tsx
@@ -14,13 +14,7 @@ const Navheader = () => {
                 : "scaleY(1)";
         }}
       >
-        <svg
-          xmlns="http://www.w3.org/2000/svg"
-          viewBox="0 0 24 24"
-          fill="rgb(255, 255, 255, 1)"
-        >
-          <path d="M3 4H21V6H3V4ZM9 11H21V13H9V11ZM3 18H21V20H3V18Z"></path>
-        </svg>
+        <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" fill="currentColor"><path d="M3 4H21V6H3V4ZM9 11H21V13H9V11ZM3 18H21V20H3V18Z"></path></svg>
       </div>
       <div className="header-nav" ref={popupRef}>
         <a href="#">О компании</a>
