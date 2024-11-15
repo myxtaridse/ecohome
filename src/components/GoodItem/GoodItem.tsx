f322486795a2616ee1f1cb23c89670fbf7af4645
myxtaridse <rustambyr69@gmail.com>
2024-11-15 14:57:28 +0300

    Error-8

--- src/components/GoodItem/GoodItem.tsx
+++ src/components/GoodItem/GoodItem.tsx
@@ -6,7 +6,7 @@ import React from 'react'
 // import GoodQuestions from './GoodQuestions/GoodQuestions'
 // import GoodReviews from './GoodReviews/GoodReviews'
 import { CustomContextProductItem } from '../../context/ProductContext';
-// import Loading from '../Loading/Loading'
+import Loading from '../Loading/Loading'
 import { CustomContextMain } from '../../context/MainContext'
 
 const GoodItem = () => {
@@ -27,73 +27,73 @@ const GoodItem = () => {
 
   console.log(valueGood);
   
-return <div>122</div>
-  // if (!valueGood) return <Loading /> 
 
-  // return (
-   
-  //     <div className="goodItem">
+  if (!valueGood) return <Loading /> 
+
+  return (
+   <div>123</div>
+      // <div className="goodItem">
         
-  //       <div className='goodItem-main-title'>
+      //   <div className='goodItem-main-title'>
         
-  //      <h1>{valueGood[0].titleGood}</h1>
-  //      <button className='goodItem-add-review'>
-  //         Добавить отзыв
-  //      </button>
-  //      </div>
-  //       <div className='goodItem-flex'>
-  //       <GoodImg setIsActive={setIsActive} />
+      //  <h1>{valueGood[0].titleGood}</h1>
+      //  <button className='goodItem-add-review'>
+      //     Добавить отзыв
+      //  </button>
+      //  </div>
+      //   <div className='goodItem-flex'>
+      //   <GoodImg setIsActive={setIsActive} />
        
-  //      <GoodParameter />
+      //  <GoodParameter />
        
-  //       <GoodAdd />
-  //       </div>
-  //       <div className='goodItem-allDescription'>
+      //   <GoodAdd />
+      //   </div>
+      //   <div className='goodItem-allDescription'>
           
-  //      <div className='goodItem-allDescription-item'>
-  //      <h1 onClick={() => {
-  //       if (isActive !== 0) {
-  //         setIsActive(0)
-  //       } else {
-  //         setIsActive(null)
-  //       }
-  //      }} className={["goodItem-allDescription-title goodItem-allDescription-title-description", isActive === 0 ? 'activeTitle' : ''].join(" ")}>ОПИСАНИЕ И ХАРАКТЕРИСТИКИ</h1>
-  //      <div className={['goodItem-allDescription-block scale-1', isActive === 0 ? 'activeBlock' : ''].join(" ")}>
-  //         <GoodDescription />
-  //         <GoodParameter />
-  //       </div>
-  //      </div>
+      //  <div className='goodItem-allDescription-item'>
+      //  <h1 onClick={() => {
+      //   if (isActive !== 0) {
+      //     setIsActive(0)
+      //   } else {
+      //     setIsActive(null)
+      //   }
+      //  }} className={["goodItem-allDescription-title goodItem-allDescription-title-description", isActive === 0 ? 'activeTitle' : ''].join(" ")}>ОПИСАНИЕ И ХАРАКТЕРИСТИКИ</h1>
+      //  <div className={['goodItem-allDescription-block scale-1', isActive === 0 ? 'activeBlock' : ''].join(" ")}>
+      //     <GoodDescription />
+      //     <GoodParameter />
+      //   </div>
+      //  </div>
   
-  //      <div className='goodItem-allDescription-item'>
-  //      <h1 onClick={() => {
-  //       if (isActive !== 1) {
-  //         setIsActive(1)
-  //       } else {
-  //         setIsActive(null)
-  //       }
-  //      }} className={["goodItem-allDescription-title goodItem-allDescription-title-reviews", isActive === 1 ? 'activeTitle' : ''].join(" ")}>ОТЗЫВЫ</h1>
-  //      <div className={['goodItem-allDescription-block scale-2', isActive === 1 ? 'activeBlock' : ''].join(" ")}>
-  //         <GoodReviews />
-  //       </div>
-  //      </div>
+      //  <div className='goodItem-allDescription-item'>
+      //  <h1 onClick={() => {
+      //   if (isActive !== 1) {
+      //     setIsActive(1)
+      //   } else {
+      //     setIsActive(null)
+      //   }
+      //  }} className={["goodItem-allDescription-title goodItem-allDescription-title-reviews", isActive === 1 ? 'activeTitle' : ''].join(" ")}>ОТЗЫВЫ</h1>
+      //  <div className={['goodItem-allDescription-block scale-2', isActive === 1 ? 'activeBlock' : ''].join(" ")}>
+      //     <GoodReviews />
+      //   </div>
+      //  </div>
   
-  //       <div className='goodItem-allDescription-item'>
-  //       <h1 onClick={() =>{
-  //         if (isActive !== 2) {
-  //           setIsActive(2)
-  //         } else {
-  //           setIsActive(null)
-  //         }
-  //       }} className={["goodItem-allDescription-title goodItem-allDescription-title-questions", isActive === 2 ? 'activeTitle' : ''].join(" ")}>ЗАДАТЬ ВОПРОС</h1>
-  //       <div className={['goodItem-allDescription-block scale-3', isActive === 2 ? 'activeBlock' : ''].join(" ")}>
-  //        <GoodQuestions />
-  //       </div>
-  //       </div>
+      //   <div className='goodItem-allDescription-item'>
+      //   <h1 onClick={() =>{
+      //     if (isActive !== 2) {
+      //       setIsActive(2)
+      //     } else {
+      //       setIsActive(null)
+      //     }
+      //   }} className={["goodItem-allDescription-title goodItem-allDescription-title-questions", isActive === 2 ? 'activeTitle' : ''].join(" ")}>ЗАДАТЬ ВОПРОС</h1>
+      //   <div className={['goodItem-allDescription-block scale-3', isActive === 2 ? 'activeBlock' : ''].join(" ")}>
+      //    <GoodQuestions />
+      //   </div>
+      //   </div>
        
         
-  //       </div>
-  //     </div>
-  //   )
+      //   </div>
+      // </div>
+    )
   
   }
 
