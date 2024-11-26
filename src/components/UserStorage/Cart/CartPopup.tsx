808e2228c04aa5f756383c798f96c72ecb32c4b3
myxtaridse <rustambyr69@gmail.com>
2024-11-26 13:35:45 +0300

    popup-cart

--- src/components/UserStorage/Cart/CartPopup.tsx
+++ src/components/UserStorage/Cart/CartPopup.tsx
@@ -19,12 +19,19 @@ const CartPopup: React.FC<CartPopupType> = ({ setIsMore }) => {
     const downMove = (e: any) => {
         if (e && isTouch) {
             const currentY = e.touches[0].clientY - startTouch;
-            setCurrentTouch(currentY);
+            // document.body.style.overflow = 'hidden'
+            console.log(currentY);
+            
+            if (currentY > 0) {
+              setCurrentTouch(currentY);
+            } 
+            
+            
         }
     }
 
     const downEnd = () => {
-        if (currentTouch > 50) {
+        if (currentTouch > 150) {
             setIsMore(false);
             setIsTouch(false);
         }
@@ -44,9 +51,11 @@ const CartPopup: React.FC<CartPopupType> = ({ setIsMore }) => {
       <div
         className="cartPopup"
         onTouchStart={(e) => downStart(e)}
+        // onTouchMove={(e) => downMove(e)}
         onTouchMove={(e) => downMove(e)}
         onTouchEnd={downEnd}
-        style={{transform: `translateY(-${currentTouch})px`}}
+
+        style={{transform: `translateY(${currentTouch}px)`}}
       >
         <div className='cartPopup-close' onClick={() => setIsMore(false)}>
             <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" fill="currentColor"><path d="M11.9997 10.5865L16.9495 5.63672L18.3637 7.05093L13.4139 12.0007L18.3637 16.9504L16.9495 18.3646L11.9997 13.4149L7.04996 18.3646L5.63574 16.9504L10.5855 12.0007L5.63574 7.05093L7.04996 5.63672L11.9997 10.5865Z"></path></svg>
