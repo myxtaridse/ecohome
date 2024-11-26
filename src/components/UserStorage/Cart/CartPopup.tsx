e9b0e176520ae2611b4f2ed0fac63d88551c4e6f
myxtaridse <rustambyr69@gmail.com>
2024-11-26 16:07:16 +0300

    scroll popup

--- src/components/UserStorage/Cart/CartPopup.tsx
+++ src/components/UserStorage/Cart/CartPopup.tsx
@@ -18,7 +18,7 @@ const CartPopup: React.FC<CartPopupType> = ({ setIsMore, isMore }) => {
       const clientY = e.touches[0].clientY;
       console.log(clientY, e.target.offsetTop);
       if (clientY && clientY > 0 && clientY < 800) {
-        e.target.style.transform = `translateY(${clientY}px)`
+        e.target.style.transform = `translateY(${clientY -  e.target.offsetTop}px)`
       }
       
     }
@@ -46,7 +46,7 @@ const CartPopup: React.FC<CartPopupType> = ({ setIsMore, isMore }) => {
         className="cartPopup"
         onTouchMove={(e) => touchEventFnc(e)}
 
-        // style={{transform: `translateY(${currentTouch}px)`}}
+        style={{transition: `.3s`}}
       >
         <div className='cartPopup-close' onClick={() => setIsMore(false)}>
             <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" fill="currentColor"><path d="M11.9997 10.5865L16.9495 5.63672L18.3637 7.05093L13.4139 12.0007L18.3637 16.9504L16.9495 18.3646L11.9997 13.4149L7.04996 18.3646L5.63574 16.9504L10.5855 12.0007L5.63574 7.05093L7.04996 5.63672L11.9997 10.5865Z"></path></svg>
