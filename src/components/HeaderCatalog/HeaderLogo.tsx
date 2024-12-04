4ddc346e1747c959044b2920498c8eacc893b51a
myxtaridse <rustambyr69@gmail.com>
2024-12-04 15:17:10 +0300

    update logo

--- src/components/HeaderCatalog/HeaderLogo.tsx
+++ src/components/HeaderCatalog/HeaderLogo.tsx
@@ -1,12 +1,13 @@
 // import React from "react";
-import logoText from "../../assets/icons/favicon/logo-main-text.svg";
-import logoImg from "../../assets/icons/favicon/logo-main-img.svg";
+// import logoText from "../../assets/icons/favicon/logo-main-text.svg";
+// import logoImg from "../../assets/icons/favicon/logo-main-img.svg";
+import logo from '../../assets/icons/favicon/logo.svg';
 
 const HeaderLogo = () => {
   return (
     <div className="header-logo">
-      <img className="header-logo-text" src={logoText} alt="logo" />
-      <img className="header-logo-img" src={logoImg} alt="logo" />
+      <img className="header-logo-text" src={logo} alt="logo" />
+      {/* <img className="header-logo-img" src={logoImg} alt="logo" /> */}
     </div>
   );
 };
