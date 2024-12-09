151e59046e8b0be697d4d3661de0b1c6cd8e87a4
myxtaridse <rustambyr69@gmail.com>
2024-12-09 14:55:46 +0300

    update good-section

--- src/components/GoodSection/GoodSection.tsx
+++ src/components/GoodSection/GoodSection.tsx
@@ -2,7 +2,7 @@ import React from 'react';
 import GoodSectionGallery from './GoodSectionGallery';
 import Rating from '../Rating';
 import { useLocation } from 'react-router-dom';
-import { CustomContextMain } from '../../context/MainContext';
+// import { CustomContextMain } from '../../context/MainContext';
 import image404 from '../../assets/images-site/404.png';
 
 // export interface GoodSectionType {
@@ -29,32 +29,31 @@ const GoodSection: React.FC<any> = ({article, titleGood, price, reviews, categor
 
     const [isBasket, setIsBasket] = React.useState(false)
 
-    // eslint-disable-next-line @typescript-eslint/no-explicit-any
-    const {setIsMore, setChildrenPopup}: any = React.useContext(CustomContextMain);
+    // const {setIsMore, setChildrenPopup}: any = React.useContext(CustomContextMain);
 
-    const morePopupFn = () => {
-        setIsMore(true)
-        setChildrenPopup(
-            <div className='cartPopup-actions'>
-                        <div className='cartPopup-item'>
-                            <div>
-                                <svg viewBox="0 0 40 40" fill="none" xmlns="http://www.w3.org/2000/svg">
-                                    <path d="M32.0703 7.92893C34.5077 10.3713 35.4239 13.7501 34.82 16.8947C35.7684 17.162 36.6639 17.667 37.4112 18.4088C39.751 20.7317 39.751 24.4978 37.4112 26.8207L28.3327 35.8333L23.296 30.8333L18.3325 35.8083L4.20165 21.655C0.69443 17.7283 0.825527 11.6984 4.59495 7.92893C8.36863 4.15525 14.408 4.02812 18.3344 7.54755C22.2494 4.03333 28.2993 4.15 32.0703 7.92893ZM21.6027 20.7743C20.576 21.7935 20.576 23.4358 21.6027 24.455L28.3327 31.1363L35.0627 24.455C36.0894 23.4358 36.0894 21.7935 35.0627 20.7743C34.0227 19.7418 32.3264 19.7418 31.2827 20.778L28.3292 23.701L25.9844 21.375L25.379 20.7743C24.339 19.7418 22.6427 19.7418 21.6027 20.7743ZM6.95197 10.286C4.46907 12.7689 4.34442 16.7455 6.63257 19.372L18.3325 31.0905L20.931 28.4867L19.2542 26.8207C16.9144 24.4978 16.9144 20.7317 19.2542 18.4088C21.594 16.086 25.3877 16.086 27.7275 18.4088L28.332 19.0085L28.9379 18.4088C29.6455 17.7063 30.4862 17.2162 31.3793 16.9387C32.072 14.6596 31.509 12.0854 29.711 10.2835C27.2125 7.7799 23.1787 7.67835 20.561 10.0281L18.3359 12.0254L16.1094 10.0297C13.4843 7.67662 9.45778 7.78013 6.95197 10.286Z" fill="#1E1E1E"/>
-                                </svg>
-                            </div>
-                            <p>Добавить в избранное</p>
-                        </div>
-                        <div className='cartPopup-item'>
-                            <div>
-                                <svg viewBox="0 0 40 40" fill="none" xmlns="http://www.w3.org/2000/svg">
-                                    <path d="M3.33398 20.0007H6.66732V35.0007H3.33398V20.0007ZM8.33398 23.334H11.6673V35.0007H8.33398V23.334ZM26.6673 13.334H30.0007V35.0007H26.6673V13.334ZM31.6673 16.6673H35.0007V35.0007H31.6673V16.6673ZM15.0007 3.33398H18.334V35.0007H15.0007V3.33398ZM20.0007 6.66732H23.334V35.0007H20.0007V6.66732Z" fill="#1E1E1E"/>
-                                </svg>
-                            </div>
-                            <p>Добавить в сравнение</p>
-                        </div>
-            </div>
-        )
-    }
+    // const morePopupFn = () => {
+    //     setIsMore(true)
+    //     setChildrenPopup(
+    //         <div className='cartPopup-actions'>
+    //                     <div className='cartPopup-item'>
+    //                         <div>
+    //                             <svg viewBox="0 0 40 40" fill="none" xmlns="http://www.w3.org/2000/svg">
+    //                                 <path d="M32.0703 7.92893C34.5077 10.3713 35.4239 13.7501 34.82 16.8947C35.7684 17.162 36.6639 17.667 37.4112 18.4088C39.751 20.7317 39.751 24.4978 37.4112 26.8207L28.3327 35.8333L23.296 30.8333L18.3325 35.8083L4.20165 21.655C0.69443 17.7283 0.825527 11.6984 4.59495 7.92893C8.36863 4.15525 14.408 4.02812 18.3344 7.54755C22.2494 4.03333 28.2993 4.15 32.0703 7.92893ZM21.6027 20.7743C20.576 21.7935 20.576 23.4358 21.6027 24.455L28.3327 31.1363L35.0627 24.455C36.0894 23.4358 36.0894 21.7935 35.0627 20.7743C34.0227 19.7418 32.3264 19.7418 31.2827 20.778L28.3292 23.701L25.9844 21.375L25.379 20.7743C24.339 19.7418 22.6427 19.7418 21.6027 20.7743ZM6.95197 10.286C4.46907 12.7689 4.34442 16.7455 6.63257 19.372L18.3325 31.0905L20.931 28.4867L19.2542 26.8207C16.9144 24.4978 16.9144 20.7317 19.2542 18.4088C21.594 16.086 25.3877 16.086 27.7275 18.4088L28.332 19.0085L28.9379 18.4088C29.6455 17.7063 30.4862 17.2162 31.3793 16.9387C32.072 14.6596 31.509 12.0854 29.711 10.2835C27.2125 7.7799 23.1787 7.67835 20.561 10.0281L18.3359 12.0254L16.1094 10.0297C13.4843 7.67662 9.45778 7.78013 6.95197 10.286Z" fill="#1E1E1E"/>
+    //                             </svg>
+    //                         </div>
+    //                         <p>Добавить в избранное</p>
+    //                     </div>
+    //                     <div className='cartPopup-item'>
+    //                         <div>
+    //                             <svg viewBox="0 0 40 40" fill="none" xmlns="http://www.w3.org/2000/svg">
+    //                                 <path d="M3.33398 20.0007H6.66732V35.0007H3.33398V20.0007ZM8.33398 23.334H11.6673V35.0007H8.33398V23.334ZM26.6673 13.334H30.0007V35.0007H26.6673V13.334ZM31.6673 16.6673H35.0007V35.0007H31.6673V16.6673ZM15.0007 3.33398H18.334V35.0007H15.0007V3.33398ZM20.0007 6.66732H23.334V35.0007H20.0007V6.66732Z" fill="#1E1E1E"/>
+    //                             </svg>
+    //                         </div>
+    //                         <p>Добавить в сравнение</p>
+    //                     </div>
+    //         </div>
+    //     )
+    // }
 
     
   return (
@@ -178,14 +177,17 @@ const GoodSection: React.FC<any> = ({article, titleGood, price, reviews, categor
                 </div>
             )
         }
-
-        <div className='goodSection-more' onClick={morePopupFn}>
+        <div className='goodSection-more'>
+            <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" fill="currentColor">
+            <path d="M12.001 4.52853C14.35 2.42 17.98 2.49 20.2426 4.75736C22.5053 7.02472 22.583 10.637 20.4786 12.993L11.9999 21.485L3.52138 12.993C1.41705 10.637 1.49571 7.01901 3.75736 4.75736C6.02157 2.49315 9.64519 2.41687 12.001 4.52853Z"></path></svg>
+        </div>
+        {/* <div className='goodSection-more' onClick={morePopupFn}>
             <svg width="23" height="12" viewBox="0 0 23 12" fill="none" xmlns="http://www.w3.org/2000/svg">
                 <path d="M5.91525 8C6.97302 8 7.83051 7.10457 7.83051 6C7.83051 4.89543 6.97302 4 5.91525 4C4.85749 4 4 4.89543 4 6C4 7.10457 4.85749 8 5.91525 8Z" stroke="black" strokeLinejoin="round"/>
                 <path d="M11.6613 8C12.7191 8 13.5766 7.10457 13.5766 6C13.5766 4.89543 12.7191 4 11.6613 4C10.6036 4 9.74609 4.89543 9.74609 6C9.74609 7.10457 10.6036 8 11.6613 8Z" stroke="black" strokeLinejoin="round"/>
                 <path d="M17.4074 8C18.4652 8 19.3227 7.10457 19.3227 6C19.3227 4.89543 18.4652 4 17.4074 4C16.3497 4 15.4922 4.89543 15.4922 6C15.4922 7.10457 16.3497 8 17.4074 8Z" stroke="black" strokeLinejoin="round"/>
             </svg>
-        </div>
+        </div> */}
 
     </div>
   )
